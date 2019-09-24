da5e257f12d522f63c52e76330f36f7275929862
Satish Polumati <satishpolumati@Naborlys-MacBook-Pro-2.local>
2019-09-24 19:27:38 -0400

    missed last commit by mistake

--- app.py
+++ app.py
@@ -14,9 +14,7 @@ jwt = JWT(app,authenticate,identity) #creates an end point "/auth"
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+
 
 api.add_resource(Store,'/store/<string:name>')
 api.add_resource(Store_list,'/stores')
@@ -24,10 +22,6 @@ api.add_resource(Item,'/item/<string:name>')
 api.add_resource(Items_list,'/items')
 api.add_resource(UserRegistration,'/register')
 
-if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
-    app.run(port=5000,debug= True)
 
 
 
